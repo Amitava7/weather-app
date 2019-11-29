667e1301499c33cdb755b1109cfa16a722217823
amitava <amitava.mozumder@citrix.com>
2019-11-29 13:41:15 +0530

    routes

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+// const geocode = require('./utils/geocode')
+// const forecast = require('./utils/forecast')
+//
+// const address = process.argv[2]
+//
+// if (!address) {
+//     console.log('Please provide an address')
+// } else {
+//     geocode(address, (error, { latitude, longitude, location }) => {
+//         if (error) {
+//             return console.log(error)
+//         }
+//
+//         forecast(latitude, longitude, (error, forecastData) => {
+//             if (error) {
+//                 return console.log(error)
+//             }
+//
+//             console.log(location)
+//             console.log(forecastData)
+//         })
+//     })
+// }
+const express = require('express');
+const app = express();
 
-const address = process.argv[2]
-
-if (!address) {
-    console.log('Please provide an address')
-} else {
-    geocode(address, (error, { latitude, longitude, location }) => {
-        if (error) {
-            return console.log(error)
-        }
-
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return console.log(error)
-            }
-
-            console.log(location)
-            console.log(forecastData)
-        })
-    })
-}
+app.get('',(req,res)=>{
+  res.send('hello world');
+})
+app.listen(8080);
